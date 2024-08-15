99071e06002d10feab2f04462bd24a5aead224b9
Jo <johj703@nate.com>
2024-08-15 13:56:11 +0900

    app 파일에서 increment 함수 추가 후 import 하고 return값에 추가한 counter 컴포넌트에 count와 increment를 props로 넘기도록 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Content from "./components/Content";
+import Counter from "./components/Counter";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const increment = () => {
+    setCount(count + 1);
+  };
+
   return (
     <>
       <Header />
-      <Content></Content>
+      <Content>
+        <Counter count={count} increment={increment} />
+      </Content>
     </>
   );
 }
