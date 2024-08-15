03fc08547b4da649c9ca1966618465847bc8c5c5
Jo <johj703@nate.com>
2024-08-15 13:50:11 +0900

    app 파일에 Content 컴포넌트 import 후 return 값에 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Header from "./components/Header";
+import Content from "./components/Content";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Header />
+      <Content></Content>
     </>
   );
 }
